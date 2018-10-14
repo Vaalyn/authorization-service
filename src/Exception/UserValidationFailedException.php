63039d3774598b15b2b642dff3483fcdf6438596
Vaalyn <vaalyndev@gmail.com>
2018-10-14 14:20:47 +0200

    Catch validation exception to create more meaningful error message and prevent output of user data on failure

---
+++ src/Exception/UserValidationFailedException.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Vaalyn\AuthorizationService\Exception;
+
+class UserValidationFailedException extends \Exception {
+
+}
--- src/Transformer/AuthorizationUserTransformer.php
+++ src/Transformer/AuthorizationUserTransformer.php
@@ -4,7 +4,9 @@ declare(strict_types = 1);
 
 namespace Vaalyn\AuthorizationService\Transformer;
 
+use Respect\Validation\Exceptions\NestedValidationException;
 use Vaalyn\AuthorizationService\AuthorizationUser;
+use Vaalyn\AuthorizationService\Exception\UserValidationFailedException;
 use Vaalyn\AuthorizationService\Validator\AuthorizationUserObjectValidator;
 
 class AuthorizationUserTransformer {
@@ -15,7 +17,18 @@ class AuthorizationUserTransformer {
 	 */
 	public function transformObjectToAuthorizationUser(object $user): AuthorizationUser {
 		$authorizationUserObjectValidator = new AuthorizationUserObjectValidator();
-		$authorizationUserObjectValidator->validate($user);
+
+		try {
+			$authorizationUserObjectValidator->validate($user);
+		} catch (NestedValidationException $exception) {
+			$excpetionMessage = sprintf(
+				'User validation for %s failed because of the following reasons: %s',
+				self::class,
+				implode(', ', $exception->getMessages())
+			);
+
+			throw new UserValidationFailedException($excpetionMessage);
+		}
 
 		return new AuthorizationUser(
 			$user->getUserId(),
